bd1efaf6d4e8ccd3166855f1738da6756d963321
Rikka丶 <128763841+RikkaLzw@users.noreply.github.com>
2023-11-24 23:11:36 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -84,5 +84,12 @@ export const DEFAULT_MODELS = [
     name: "gpt-3.5-turbo-16k",
     available: true,
   },
-  
+  {
+    name: "gpt-4-0613",
+    available: true,
+  },
+   {
+    name: "gpt-4-1106-preview",
+    available: true,
+  },
 ] as const;
