d2408c1a2c2330f8d51dc13c38ecc746ae23aa7c
Rikka <lizw1017@gmail.com>
2026-01-13 15:04:07 +0800

    修改模型列表

--- app/store/config.ts
+++ app/store/config.ts
@@ -64,8 +64,8 @@ export const DEFAULT_CONFIG = {
   models: DEFAULT_MODELS as any as LLMModel[],
 
   modelConfig: {
-    model: "gpt-4o-mini" as ModelType,
-    providerName: "OpenAI" as ServiceProvider,
+    model: "claude-opus-4-5-20251101" as ModelType,
+    providerName: "Anthropic" as ServiceProvider,
     temperature: 0.5,
     top_p: 1,
     max_tokens: 4000,
@@ -191,7 +191,7 @@ export const useAppConfig = createPersistStore(
       }));
     },
 
-    allModels() {},
+    allModels() { },
   }),
   {
     name: StoreKey.Config,
